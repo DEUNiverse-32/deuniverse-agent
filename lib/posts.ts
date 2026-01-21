192190d85d43518be3a95cd9213f87e178147397
deuniverse <ay7092@gmail.com>
2026-01-21 17:30:57 +0900

    Update posts.ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,9 +1,9 @@
 // lib/posts.ts
-import { russian-roulette } from './posts/russian-roulette';
+import { russianRoulette } from './posts/russianroulette';
 
 // 여기서 대괄호 []를 사용해 기록들을 하나로 묶어주는 거야.
 export const posts = [
-  russian-roulette,
+  russianRoulette,
 ];
 
 export function getAllPosts() {
@@ -14,3 +14,4 @@ export function getPostBySlug(slug: string | string[] | undefined) {
   const actualSlug = Array.isArray(slug) ? slug[0] : slug;
   return posts.find((p) => p.slug === actualSlug);
 }
+
